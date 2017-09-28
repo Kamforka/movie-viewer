a3ed560242fd927fc2308775bfa2559e56f1e236
Antal <szabolcs.antal@evopro-group.com>
2017-09-28 17:55:28 +0200

    Setup app module and component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './app.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { RouterStoreModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    StoreModule.provideStore(reducer),
+    // RouterStoreModule.connectRouter(),
+    StoreDevtoolsModule.instrumentOnlyWithExtension({
+      maxAge: 5
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
